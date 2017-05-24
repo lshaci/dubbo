f1457613bb9353088e2fe44fe4bf3f284de191ef
lshaci <lshaci@qq.com>
2017-05-24 16:47:08 +0800

    初始化

---
+++ dubbo-domain/src/main/java/com/lshaci/dubbo/domain/City.java
@@ -0,0 +1,29 @@
+package com.lshaci.dubbo.domain;
+
+import com.lshaci.dubbo.domain.base.IDEntity;
+
+public class City extends IDEntity<Long> {
+	
+	private String name;
+	
+	private String state;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	@Override
+	public String toString() {
+		return "City [id=" + super.getId() + ", name=" + name + ", state=" + state + "]";
+	}
+	
+}
