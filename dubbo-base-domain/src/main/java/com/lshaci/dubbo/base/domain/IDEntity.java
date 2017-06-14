44bbbabbfe6b7a62f8b34abec7c5458828981417
lshaci <lshaci@qq.com>
2017-06-14 11:23:21 +0800

    Base for domain

---
+++ dubbo-base-domain/src/main/java/com/lshaci/dubbo/base/domain/IDEntity.java
@@ -0,0 +1,27 @@
+package com.lshaci.dubbo.base.domain;
+
+import java.io.Serializable;
+
+/**
+ * 公共的ID实体
+ * 
+ * @author lshaci
+ *
+ * @param <ID>		实体主键的类型
+ */
+public abstract class IDEntity<ID extends Serializable> implements Serializable  {
+
+	private static final long serialVersionUID = -2044745800861094697L;
+	
+	private ID id;
+
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+	
+	
+}
